c02d9508a5beae430052a5166eb3869f2dcca113
Lucas Braga <bragaluka19@gmail.com>
2026-02-03 22:31:53 -0300

    feat(router): atualizar index com rotas da home e /operadoras

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 import Operadoras from '../views/Operadoras.vue'
 import OperadoraDetalhe from '../views/OperadoraDetalhe.vue'
 import Dashboard from '../views/Dashboard.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Operadoras },
+    { path: '/', component: Home },
+    { path: '/operadoras', component: Operadoras },
     { path: '/operadoras/:cnpj', component: OperadoraDetalhe },
     { path: '/dashboard', component: Dashboard },
+
+    // opcional: redireciona qualquer rota inválida para Home
+    { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
 })
